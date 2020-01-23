f03baf354223a34aec931bde918edce31fa9890f
jfcross4 <jfcross4@gmail.com>
2020-01-22 22:42:36 -0500

    added detect cores

--- pitchRx Graphing.R
+++ pitchRx Graphing.R
@@ -6,6 +6,11 @@ library(pitchRx)
 library(mgcv)
 library(baseballr)
 library(readr)
+library(parallel)
+
+if (detectCores()>2 & !is.na(detectCores())) {
+  cl <- makeCluster(detectCores()-2)
+} else cl <- NULL
 
 umpire_data_url <- "https://t.co/R17oTRDnUN"
 umpires <- read.csv(umpire_data_url)
@@ -19,14 +24,21 @@ noswing$strike <- as.numeric(noswing$description %in% "called_strike")
 noswing <- noswing %>% rename(px = plate_x, pz = plate_z)
 
 noswing_2016_06_03 <- noswing %>% filter(game_date == "2016-06-03")
+noswing_2016_06_03$umpire_name <- droplevels(noswing_2016_06_03$umpire_name)
 
-m <- bam(strike ~ s(px, pz, by = factor(umpire_name)) +
-           factor(umpire_name), data = noswing_2016_06_03, 
-         family = binomial(link = 'logit'))
+# m <- bam(strike ~ s(px, pz, by = factor(umpire_name)) +
+#            factor(umpire_name), data = noswing_2016_06_03, 
+#          family = binomial(link = 'logit'), cluster=cl)
+
+m <- bam(strike ~ s(px, pz, by = umpire_name,k=10) , data = noswing_2016_06_03, 
+         family = binomial(link = 'logit'), cluster=cl)
+
+
 x <- list(
-  facet_wrap( ~  umpire_name),
+  facet_grid(. ~  umpire_name),
   theme_bw(),
   coord_equal(),
   viridis::scale_fill_viridis(name = "Probability of Called Strike Based on Umpire")
 )
 strikeFX(noswing_2016_06_03, model = m, layer = x, draw_zones = TRUE)
+
